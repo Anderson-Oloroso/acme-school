e8a6baf59ff2afe0b02d821dbfc00a1f299971ae
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-15 11:04:47 -0600

    :sparkles: feat(Course): Create course file with its module

---
+++ src/models/course.js
@@ -0,0 +1,12 @@
+class Course{
+    constructor(id = null, code, description, intensity, weight, active){
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.intensity = intensity;
+        this.weight = weight;
+        this.active = active;
+    }
+}
+
+module.exports == { Course };
