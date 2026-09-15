d3bf3f04e2a825fc5da9cc41c64ccd588a08a551
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-15 10:46:46 -0600

    :sparkles: feat(Person): Create person file with its module

---
+++ src/models/person.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(id = null, firstName, lastName, idtTypeId, identificationNumber, email){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idtTypeId = idtTypeId;
+        this.identificationNumber = identificationNumber;
+        this.email = email;
+    }
+}
+
+module.exports == { Person };
