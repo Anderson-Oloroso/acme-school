9a0df0d6dacd3fee5c761591493f3e6172640b66
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-15 11:08:22 -0600

    :sparkles: feat(Topic): Create topic file with its module

---
+++ src/models/topic.js
@@ -0,0 +1,12 @@
+class Topic{
+    constructor(id = null,courseId, code, title, description, active){
+        this.id = id;
+        this.courseId = courseId;
+        this.code = code;
+        this.title = title;
+        this.description = description;
+        this.active = active;
+    }
+}
+
+module.exports == { Topic };
