d449b3be2080c9652df4b8bc611fb7f94d3875a3
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-15 11:06:56 -0600

    :sparkles: feat(Clasroom): Create classroom file with its module

---
+++ src/models/classroom.js
@@ -0,0 +1,11 @@
+class Classroom{
+    constructor(id = null, code, description, capacity, active){
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.capacity = capacity;
+        this.active = active;
+    }
+}
+
+module.exports == { Classroom };
