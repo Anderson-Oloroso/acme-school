a4c2f5f8fc5a166e8b0ac95a800eaaf202c14ea5
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-15 10:22:31 -0600

    :sparkles: feat(app): Add CRUD classes to project

--- src/app.js
+++ src/app.js
@@ -123,3 +123,224 @@ class Connection{
 }
 
 Connection.close();
+
+class IdentificationTypeMethods{
+    create(idtTypes){
+        console.log('[IDENTIFICATION_TYPE] -> Guardando registro de Tipo de identificación ...');
+    }
+
+    findAll(){
+        console.log('[IDENTIFICATION_TYPE] -> Consultando todos los registros de Tipo de identificación ...');
+    }
+
+    findOne(id){
+        console.log(`[IDENTIFICATION_TYPE] -> Consultando registro de Tipo de identificación con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[IDENTIFICATION_TYPE] -> Actualizando registro de Tipo de identificación con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[IDENTIFICATION_TYPE] -> Eliminando registro de Tipo de identificación con id: ${id} ...`);
+    }
+}
+ 
+
+class StudentMethods{
+    create(student){
+        console.log('[STUDENT] -> Guardando registro de Estudiante...');
+    }
+
+    findAll(){
+        console.log('[STUDENT] -> Consultando todos los registros de Estudiante ...');
+    }
+
+    findOne(id){
+        console.log(`[STUDENT] -> Consultando registro de Estudiante con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[STUDENT] -> Actualizando registro de Estudiante con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[STUDENT] -> Eliminando registro de Estudiante con id: ${id} ...`);
+    }
+}
+
+class TeacherMethods{
+    create(teacher){
+        console.log('[TEACHER] -> Guardando registro de Docente ...');
+    }
+
+    findAll(){
+        console.log('[TEACHER] -> Consultando todos los registros de Docente ...');
+    }
+
+    findOne(id){
+        console.log(`[TEACHER] -> Consultando registro de Docente con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[TEACHER] -> Actualizando el registro de Docente con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[TEACHER] -> Eliminando registro de Docente con id: ${id} ...`);
+    }
+}
+
+class CityMethods{
+    create(city){
+        console.log('[CITY] -> Guardando registro de Ciudad ...');
+    }
+
+    findAll(){
+        console.log(`[CITY] -> Consultando todos los registros de Ciudad ...`);
+    }
+
+    findOne(id){
+        console.log(`[CITY] -> Consultando registro de Ciudad con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[CITY] -> Actualizando registro de Ciudad con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[CITY] -> Eliminando registro de Ciudad con id: ${id} ...`);
+    }
+}
+
+class RateMethods{
+    create(rate){
+        console.log('[RATE] -> Guardando registro de Calificación ...');
+    }
+
+    findAll(){
+        console.log('[RATE] -> Consultando todos los registros de Calificación ...');
+    }
+
+    findOne(id){
+        console.log(`[RATE] -> Consultando registro de Calificación con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[RATE] -> Actualizando registro de Calificación con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[RATE] -> Eliminando registro de Calificación con id: ${id} ...`);
+    }
+}
+
+class InscriptionMethods{
+    create(inscription){
+        console.log('[INSCRIPTION] -> Guardando registro de Inscripción ...');
+    }
+
+    findAll(){
+        console.log('[INSCRIPTION] -> Consultando todos los registros de Inscripción ...');
+    }
+
+    findOne(id){
+        console.log(`[INSCRIPTION] -> Consultando registro de Inscripción con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[INSCRIPTION] -> Actualizando registro de Inscripción con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[INSCRIPTION] -> Eliminando registro de Inscripción con id: ${id} ...`);
+    }
+}
+
+class CourseScheduleMethods{
+    create(courseSchedule){
+        console.log('[COURSE_SCHEDULE] -> Guardando registro de Horario de Curso ...');
+    }
+
+    findAll(){
+        console.log('[COURSE_SCHEDULE] -> Consultando todos los registros de Horario de Curso ...');
+    }
+
+    findOne(id){
+        console.log(`[COURSE_SCHEDULE] -> Consultando registro de Horario de Curso con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[COURSE_SCHEDULE] -> Actualizando registro de Horario de Curso con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[COURSE_SCHEDULE] -> Eliminando registro de Horario de Curso con id: ${id} ...`);
+    }
+}
+
+class CourseMethods{
+    create(course){
+        console.log('[COURSE] -> Guardando registro de Curso ...');
+    }
+
+    findAll(){
+        console.log('[COURSE] -> Consultando todos los registros de Curso ...');
+    }
+
+    findOne(id){
+        console.log(`[COURSE] -> Consultando registro de Curso con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[COURSE] -> Actualizando registro de Curso con id: ${id} ...`);   
+    }
+
+    delete(id){
+        console.log(`[COURSE] -> Eliminando registro de Curso con id: ${id} ...`);
+    }
+}
+
+class ClassroomMethods{
+    create(classrom){
+        console.log('[CLASSROOM] -> Guardando registro de Classroom ...');
+    }
+
+    findAll(){
+        console.log('[CLASSROOM] -> Consultando todos los registros de Classroom ...');
+    }
+
+    findOne(id){
+        console.log(`[CLASSROOM] -> Consultando registro de Classrooms con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[CLASSROOM] -> Actualizando registro de Classroom con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[CLASSROOM] -> Eliminando registro de Classroom con id: ${id} ...`)
+    }
+}
+
+class TopicMethods{
+    create(topic){
+        console.log('[TOPIC] -> Guardando registro de Topic ...');
+    }
+
+    findAll(){
+        console.log('[TOPIC] -> Consultando todos los registros de Topic ...');
+    }
+
+    findOne(id){
+        console.log(`[TOPIC] -> Consultando el registro de Topic con id: ${id} ...`);
+    }
+
+    update(id, newRecord){
+        console.log(`[TOPIC] -> Actualizando registro de Topic con id: ${id} ...`);
+    }
+
+    delete(id){
+        console.log(`[TOPIC] -> Eliminando registro de Topic con id: ${id} ...`);
+    }
+} 
