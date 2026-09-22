996021932338f96b895379f22b8737720a77b5ca
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-21 22:15:50 -0600

    :sparkles: feat(baseRepo): Add baseRepository for extends

--- src/repositories/baseRepository.js
+++ src/repositories/baseRepository.js
@@ -1,46 +1,27 @@
+import { connection } from '../utils/dbConnection.js'
+
 class BaseRepository{
     constructor(entity){
         this.entityName = entity;
-        this.items = [];
+        this.pool = connection;
     }
 
-    create(item){
-        this.items.push(item);
-        console.log(`[${this.entityName.toUpperCase()}] -> Registro guardado exitosamente ...`);
-        return item;
-    }
-
-    list(){
+    async list(){
         console.log(`[${this.entityName.toUpperCase()}] -> Listando todos los registros ...`);
-        return this.items;
+        const [ data ] = await this.pool.query(`SELECT * FROM ${this.entityName}`);
+        return data;
     }
 
-    findOne(id){
+    async findOne(id){
         console.log(`[${this.entityName.toUpperCase()}] -> Buscando el registro con id: ${id} ...`);
-        return this.items.find(itm => itm.id === id) || null;
+        const [ data ] = await this.pool.query(`SELECT * FROM ${this.entityName} WHERE id = ${id}`);
+        return data[0] || null;
     }
 
-    update(id, newRecord){
-        console.log(`[${this.entityName.toUpperCase()}] -> Actualizando el registro con id: ${id} ...`);
-        const index = this.items.findIndex(itm => itm.id === id);
-
-        if(index !== -1){
-            this.items[index] = { ...this.items[index], ...newRecord, id};
-            return this.items[index];
-        }else{
-            return null
-        }
-    }
-
-    delete(id){
+    async delete(id){
         console.log(`[${this.entityName.toUpperCase()}] -> Eliminando el registro con id: ${id} ...`);
-        const index = this.items.findIndex(itm => itm.id === id);
-
-        if(index !== -1){
-            return this.items.splice(index, 1)[0];
-        }else{
-            return null
-        }
+        const [ result ] = await this.pool.execute(`DELETE FROM ${this.entityName} WHERE id = ?`, [id]);
+        return result.insertId;
     }
 }
 
